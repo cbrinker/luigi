48f0c5aade9bb7ffccf178c8ec4cb0ec9343bfd2
Chris Brinker <chris.brinker@gmail.com>
2014-05-15 23:49:41 +0000

    Now using supervisord to control the process, so dont need the init script to auto-start after reboot

--- rpm/build_rpm.sh
+++ rpm/build_rpm.sh
@@ -7,8 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
 python setup.py bdist_rpm \
+  --release 2 \
   --requires=python-argparse,python-tornado,python-mechanize,python-daemon \
-  --post-install="rpm/postinstall.sh"  \
-  --pre-uninstall="rpm/preuninstall.sh"  \
+#  --post-install="rpm/postinstall.sh"  \
+#  --pre-uninstall="rpm/preuninstall.sh"  \
 
 cd -
